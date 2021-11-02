cebfa5cb2b9ca13683c7209b1e9198cb531f94da
SARAV <sarav023b@gmail.com>
2021-11-03 01:00:37 +0530

    Changed ui in frontend

--- back-end/middleware/auth_middleware.js
+++ back-end/middleware/auth_middleware.js
@@ -1,30 +1,23 @@
-const fastifyPlugin = require('fastify-plugin');
-const User = require('../models/user')
+const fastifyPlugin = require("fastify-plugin");
+const User = require("../models/user");
 
-module.exports = fastifyPlugin(async (fastify)=>{
-    fastify.decorate('jwtauth', async (req,res)=>{
-        try {
-            await req.jwtVerify();
-        } catch (error) {
-            res.send(error);
-        }
-    })
-    fastify.decorate('isAdmin',async (req,reply)=>{
-        const id = usereq.params.id
-            const admin = await  User.findById(courseid)
-            console.log(admin.role)
-            
+module.exports = fastifyPlugin(async (fastify) => {
+  fastify.decorate("jwtauth", async (req, res) => {
+    try {
+      await req.jwtVerify();
+    } catch (error) {
+      res.send(error);
+    }
+  });
+  fastify.decorate("isAdmin", async (req, reply) => {
+    const id = usereq.params.id;
+    const admin = await User.findById(courseid);
+    console.log(admin.role);
 
-        
-            if (admin.role === 0) {
-                return reply.status(200).send({
-                  message: "You are  not ADMIN, Access deined"
-                });
-              }
-              
-            
-           
-        
-    })
-
-})
+    if (admin.role === 0) {
+      return reply.status(200).send({
+        message: "You are  not ADMIN, Access deined",
+      });
+    }
+  });
+});
